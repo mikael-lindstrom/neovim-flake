2905a28fc0bffbbd62accea31b3ed17da23333c6
Mikael Lindström <mikael.lindstrom@gmail.com>
2025-05-15 22:56:07 +0200

    Enable copilot for git commit messages

--- lua/neovim-config/plugins/copilot.lua
+++ lua/neovim-config/plugins/copilot.lua
@@ -2,6 +2,7 @@ require('copilot').setup({
     suggestions = { enabled = false },
     panel = { enabled = false },
     filetypes = {
+        gitcommit = true,
         yaml = true,
         markdown = true,
         help = true,
