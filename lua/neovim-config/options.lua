780dba980203b172d9b30235a4f251fe45f468a7
Mikael Lindström <mikael.lindstrom@gmail.com>
2026-01-03 23:04:43 +0100

    Update undo directory

--- lua/neovim-config/options.lua
+++ lua/neovim-config/options.lua
@@ -17,7 +17,7 @@ vim.opt.ruler = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undodir = vim.fn.stdpath('data') .. '/undodir'
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
