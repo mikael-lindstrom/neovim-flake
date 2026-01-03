ed28fe0a5ff9789d0403087a77211c0c42497bc1
Mikael Lindström <mikael.lindstrom@gmail.com>
2026-01-03 22:48:46 +0100

    Update nvim-treesitter configuration for new version

--- lua/neovim-config/plugins/treesitter.lua
+++ lua/neovim-config/plugins/treesitter.lua
@@ -1,22 +1,6 @@
-require('nvim-treesitter.configs').setup({
-    ensure_installed = {},
-    ignore_install = { 'all' },
-    sync_install = false,
-    auto_install = false,
-
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<c-backspace>',
-        },
-    },
-})
 vim.treesitter.language.register('river', 'alloy')
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'alloy' },
+    callback = function() vim.treesitter.start() end,
+})
