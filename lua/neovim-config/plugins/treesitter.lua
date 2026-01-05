876dc82f54c4404992c2809afee4b6b3759f552a
Mikael Lindström <mikael.lindstrom@gmail.com>
2026-01-05 22:34:17 +0100

    Ensure treesitter is used if available

--- lua/neovim-config/plugins/treesitter.lua
+++ lua/neovim-config/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 vim.treesitter.language.register('river', 'alloy')
 
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'alloy' },
-    callback = function() vim.treesitter.start() end,
+    pattern = '*',
+    callback = function()
+        local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+        if lang and vim.treesitter.language.add(lang) then
+            vim.treesitter.start()
+        end
+    end,
 })
